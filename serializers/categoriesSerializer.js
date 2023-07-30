51c6c838e88a4c797bc69bd4da0e331d3e71cc8c
Nely Rambelomanana <nely.rambelomanana@clirisgroup.com>
2023-07-30 18:57:19 +0200

    refactor serializer

--- serializers/categoriesSerializer.js
+++ serializers/categoriesSerializer.js
@@ -1,17 +1,26 @@
-// Serializer function to format categories data for API responses
-exports.serializeCategories = (categories) => {
+// Base serializer function to format categories data for API responses
+const baseSerializeCategories = (category) => {
     return {
-        id: categories.id,
-        name: categories.name
+        id: category.id,
+        name: category.name,
     };
 };
 
 // Serializer function to format categories with children for API responses
-exports.serializeCategoriesWithChildren = (category) => {
-    const { id, name, children } = category;
-    return {
-        id,
-        name,
-        children: children.map((child) => ({ id: child.id, name: child.name })),
-    };
+const serializeCategoriesWithChildren = (category) => {
+    const serializedCategory = baseSerializeCategories(category);
+
+    if (category.children) {
+        serializedCategory.children = category.children.map((child) => ({
+            id: child.id,
+            name: child.name,
+        }));
+    }
+
+    return serializedCategory;
+};
+
+module.exports = {
+    serializeCategories: baseSerializeCategories,
+    serializeCategoriesWithChildren: serializeCategoriesWithChildren,
 };
